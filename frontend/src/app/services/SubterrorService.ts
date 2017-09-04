f171e9830bca934e5e98bd987103dd08611b7676
leo guanco <leo.guanco@gmail.com>
2017-09-04 00:35:02 -0300

    Se agregaron las peticiones para consultar la api desde el servicio

--- frontend/src/app/services/SubterrorService.ts
+++ frontend/src/app/services/SubterrorService.ts
@@ -15,9 +15,19 @@ export class SubterrorService {
 
   constructor(private _http: Http){}
 
-  getPosts(){
+  getEvents(){
     // petición por get a esa url de un api rest de pruebas
-    return this._http.get("https://jsonplaceholder.typicode.com/posts")
+    return this._http.get("/api/event")
+      .map(res => res.json());
+  }
+
+  getStations(){
+    return this._http.get("/api/station")
+      .map(res => res.json());
+  }
+
+  getEventType(){
+    return this._http.get("/api/eventtype")
       .map(res => res.json());
   }
 
